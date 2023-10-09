c63cc3131fc22dca2291ce5941d6bb52dbfeec8d
Arian <arian.kulmer@web.de>
2023-10-09 22:11:50 +0530

    tb-common: Build new aidl qti power HAL
    Change-Id: I4bdea354e54d34d0dfe9b811e228fc6f8762d2ed
    Signed-off-by: Immanuel Raj <iamimmanuelraj@gmail.com>

--- tb.mk
+++ tb.mk
@@ -266,7 +266,7 @@ PRODUCT_COPY_FILES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.2-service-qti \
+    android.hardware.power-service-qti \
     vendor.qti.hardware.perf@1.0
 
 PRODUCT_COPY_FILES += \
