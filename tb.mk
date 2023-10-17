a368b9229695e69007c453afe63f6157da7aa114
LuK1337 <priv.luk@gmail.com>
2023-10-17 22:00:15 +0530

    tb-common: Build libhidltransport/libhwbinder
    * Vendor version
    * Needed for old blobs
    
    Change-Id: I43fc0e46547dfc126a4b2ac9dcc82c7c4f2ee172
    
    Signed-off-by: Immanuel Raj <iamimmanuelraj@gmail.com>

--- tb.mk
+++ tb.mk
@@ -172,7 +172,9 @@ PRODUCT_PACKAGES += \
     android.hidl.base@1.0 \
     android.hidl.manager@1.0 \
     libhidltransport \
-    libhwbinder
+    libhidltransport.vendor \
+    libhwbinder \
+    libhwbinder.vendor
 
 # IMS
 PRODUCT_PACKAGES += \
