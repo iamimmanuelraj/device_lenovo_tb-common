e299b65fa13857c6b3409c2b2279f5f9c374f8a9
Ethan Chen <intervigil@gmail.com>
2023-10-18 15:05:02 +0530

    tb-common: Remove unnecessary netutils packages
    * These are already packaged by AOSP.
    
    Change-Id: I77f91fbbb33a6d891d14fe852b094ab914ca3111
    
    Signed-off-by: Immanuel Raj <iamimmanuelraj@gmail.com>

--- tb.mk
+++ tb.mk
@@ -218,10 +218,7 @@ PRODUCT_COPY_FILES += \
 
 # Net
 PRODUCT_PACKAGES += \
-    android.system.net.netd@1.0 \
-    libandroid_net \
-    libnl \
-    netutils-wrapper-1.0
+    libnl
 
 # Lights
 PRODUCT_PACKAGES += \
